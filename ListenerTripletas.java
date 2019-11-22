c468362c70093bb4be84ccba384b7e3ede8421dd
Germán Esteban <38075180+German7463@users.noreply.github.com>
2019-11-22 13:45:36 +0100

    Create ListenerTripletas.java

---
+++ ListenerTripletas.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.atn.*;
+
+public class ListenerTripletas extends GramJInsBaseListener {
+
+    private ArrayList<Integer> sentencia = new ArrayList<Integer>();
+
+    private ArrayList<ArrayList<Integer>> arrays = new ArrayList<ArrayList<Integer>>();
+    private int i = 0;
+
+    public ArrayList<ArrayList<Integer>> getArray() {
+        return arrays;
+    }
+
+    private void meteValor(int valor) {
+        sentencia.add(valor);
+    }
+
+    private void meteArray(ArrayList<Integer> arr) {
+        arrays.add(arr);
+    }
+
+    @Override
+    public void visitTerminal(TerminalNode node) {
+        switch (node.getSymbol().getType()) {
+        case 9:
+            meteValor(Integer.parseInt(node.getText()));
+            System.out.println("mete número: " + node.getText());
+
+            break;
+        }
+
+    }
+
+    @Override
+    public void exitLinea(GramJInsParser.LineaContext ctx) {
+        System.out.println("exitLinea");
+        ArrayList<Integer> aux = new ArrayList<Integer>(sentencia);
+        meteArray(aux);
+        sentencia.clear();
+        
+    }
+
+}
