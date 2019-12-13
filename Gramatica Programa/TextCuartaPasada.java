82e132adb6d0be79588839a6c69cc6fc3a3ea5df
Sebitasidk <38075235+Sebitasidk@users.noreply.github.com>
2019-12-13 11:59:18 +0100

    CP

---
+++ Gramatica Programa/TextCuartaPasada.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import org.antlr.v4.runtime.tree.*;
+import org.antlr.v4.runtime.*;    
+
+public class TextCuartaPasada{
+public static void main(String[] args)throws Exception{
+
+    String inputFile=null;
+    if(args.length>0) inputFile=args[0];
+    InputStream is =System.in;
+    if(inputFile!=null){
+    is=new FileInputStream(inputFile);
+    }
+
+    ANTLRInputStream input = new ANTLRInputStream(is);
+    GramProgLexer lexer=new GramProgLexer(input);
+    CommonTokenStream tokens= new CommonTokenStream(lexer);
+    GramProgParser parser= new GramProgParser (tokens);
+    parser.setBuildParseTree(true);
+    ParseTree tree=parser.prog();
+    //System.out.println(tree.toStringTree(parser)+"no se que imprimo");
+    CuartaPasada nv=new CuartaPasada();
+     nv.visit(tree);
+     nv.ImprimirTriple();
+
+}
+};
