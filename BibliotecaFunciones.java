8a5b72c8cddf6cd8fcbcf2f8fc0c33f6095ed031
Tulkaj <38067963+Tulkaj@users.noreply.github.com>
2019-12-15 18:34:36 +0100

    Añadida biblioteca de funciones

---
+++ BibliotecaFunciones.java
@@ -0,0 +1,83 @@
+import java.util.ArrayList;
+
+public class BibliotecaFunciones{
+
+    private ArrayList<String[]> tripleta = new ArrayList<String[]>();
+    private ArrayList<String> funciones = new ArrayList<String>();;
+    private ArrayList<Entrada> pila;
+    
+    public BibliotecaFunciones(ArrayList<Entrada> pila){
+        this.pila = pila;
+        funciones.add("imprimir");
+        funciones.add("fopen");
+        funciones.add("fwrite");
+        funciones.add("freadAll");
+        funciones.add("fclose");
+        funciones.add("cadenaDonde");
+        funciones.add("cadenaSinEspacios");
+        funciones.add("cadenaSustituida");
+        funciones.add("cadenaIzquierda");
+        funciones.add("cadenaDerecha");
+        funciones.add("cadenaDentro");
+        funciones.add("cadenaLongitud");
+    }
+    public boolean existe(String nombre){
+        return funciones.contains(nombre);
+    }
+    public ArrayList<String[]> imprimir(){
+        tripleta.clear();
+        String[] tri = {"","",""};
+        tri[0] = "5";
+        tri[1] = Integer.toString(pila.size()-1);
+        tri[2] = "1";
+        tripleta.add(tri);
+        String[] tri2 = {"",""};
+        tri2[0] = "40";
+        tri2[1] = "1";
+        tripleta.add(tri2);
+        return tripleta;
+    }
+    public ArrayList<String[]> fopen(){
+        tripleta.clear();
+        String[] tri = {"","",""};
+        tri[0] = "5";
+        tri[1] = Integer.toString(pila.size()-1);
+        tri[2] = "1";
+        tripleta.add(tri);
+        String[] tri2 = {"",""};
+        tri2[0] = "40";
+        tri2[1] = "1";
+        tripleta.add(tri2);
+        return tripleta;
+    }
+    public ArrayList<String[]> fwrite(){
+        
+    }
+    public ArrayList<String[]> fclose(){
+        
+    }
+    public ArrayList<String[]> freadAll(){
+        
+    }
+    public ArrayList<String[]> cadenaDonde(){
+        
+    }
+    public ArrayList<String[]> cadenaSinEspacios(){
+        
+    }
+    public ArrayList<String[]> cadenaSustituida(){
+        
+    }
+    public ArrayList<String[]> cadenaIzquierda(){
+        
+    }
+    public ArrayList<String[]> cadenaDerecha(){
+        
+    }
+    public ArrayList<String[]>cadenaDentro(){
+
+    }
+    public ArrayList<String[]> cadenaLongitud(){
+
+    }
+}
