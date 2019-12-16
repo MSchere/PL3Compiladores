190944712bdc1cd3b350543fde85cd4649e1b110
Sebitasidk <38075235+Sebitasidk@users.noreply.github.com>
2019-12-16 16:16:18 +0100

    No se

--- CuartaPasada.java
+++ CuartaPasada.java
@@ -28,30 +28,34 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
             file.createNewFile();
             FileWriter fw = new FileWriter(file, true);
             BufferedWriter bw = new BufferedWriter(fw);
-        
-        for (int j = 0; j < Tripleeeee.size(); j++) {
-            String[] PrintPOS;
-            PrintPOS = Tripleeeee.get(j);
-            System.out.println(" ");
 
-            for (int i = 0; i < PrintPOS.length; i++) {
+            for (int j = 0; j < Tripleeeee.size(); j++) {
+                String[] PrintPOS;
+                PrintPOS = Tripleeeee.get(j);
+                System.out.println(" ");
 
-                System.out.print(PrintPOS[i] + ",");
-            }bw.write("[");
                 for (int i = 0; i < PrintPOS.length; i++) {
-                    if(i!=PrintPOS.length-1){
-                        
-                        bw.write(PrintPOS[i]+",");}
-                    else{
+
+                    System.out.print(PrintPOS[i] + ",");
+                }
+                bw.write("[");
+                for (int i = 0; i < PrintPOS.length; i++) {
+                    if (i != PrintPOS.length - 1) {
+
+                        bw.write(PrintPOS[i] + ",");
+                    } else {
                         bw.write(PrintPOS[i]);
                     }
-                }bw.write("]");
+                }
+                bw.write("]");
                 bw.write("\n");
-                    
-        }bw.close();
-    } catch (IOException e) {
-        System.err.println("Problem writing to the file statsTest.txt");
-    }}
+
+            }
+            bw.close();
+        } catch (IOException e) {
+            System.err.println("Problem writing to the file statsTest.txt");
+        }
+    }
 
     public String visitFunc(GramProgParser.FuncContext ctx) {
         return visitChildren(ctx);
@@ -135,9 +139,15 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
     public String visitDeclararYasign(GramProgParser.DeclararYasignContext ctx) {
         String[] PrintPOS = { " ", " ", " " };
         PrintPOS[0] = "1";
+        PrintPOS[1] = ctx.tipo().getText();
         switch (ctx.tipo().getText()) {
         case ("numero"):
-            PrintPOS[1] = "1";
+            if (ctx.expr().getText().contains(".")) {
+                PrintPOS[1] = "1";
+            } else {
+                PrintPOS[1] = "0";
+                ;
+            }
             break;
         case ("cadena"):
             PrintPOS[1] = "2";
@@ -157,6 +167,7 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
         switch (ctx.tipo().getText()) {
         case ("numero"):
             PrintPOS[1] = "1";
+
             break;
         case ("cadena"):
             PrintPOS[1] = "2";
@@ -173,8 +184,10 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
     }
 
     public String visitAsignar(GramProgParser.AsignarContext ctx) {
-       // ctx.ID().getText(); me pillo el nombre, se busca en la tabla de simbolos y se pilla el desplazamiento
-       // hay que ir a la posicion de la pila y cambiar el valor de la variable
+        // ctx.ID().getText(); me pillo el nombre, se busca en la tabla de simbolos y se
+        // pilla el desplazamiento
+        // hay que ir a la posicion de la pila y cambiar el valor de la variable
+        System.out.println("Estoy en asignacion " + ctx.ID());
         return visitChildren(ctx);
     }
 
@@ -183,22 +196,24 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
     }
 
     public String visitLlamadaFunc(GramProgParser.LlamadaFuncContext ctx) {
-        //comprobar que este la funcion en la tabla del simbolos y empezamos
-        // tabla de simbolos y pillar  de la funcion .getNumerosDeInputs y .getNumerosDeOutputs
-        String[] PrintPOS = { " ", " ", " " };
+        // comprobar que este la funcion en la tabla del simbolos y empezamos
+        // tabla de simbolos y pillar de la funcion .getNumerosDeInputs y
+        // .getNumerosDeOutputs
+        // String[] PrintPOS = { " ", " ", " " };
 
-        //PrintPOS[0]="1";//PUSH
-        //PrintPOS[1]=; tipo de retorno de la funcion
-        //PrintPOS[2]= " "; valor de la variable temporal de retorno que se modificara cuando se devuelva de la funcion
-        //Tripleeeee.add(PrintPOS);
-        //for de .getNumerosDeinpu
-        //PrintPOS[0]="1"
-        //PrintPOS[1]=; tipo de cada variable
-        //PrintPOS[2]=; valor de las variables
-        //Tripleeeee.add(PrintPOS);
-        System.out.println("He acabado de hacer la preparacion de las variables");
-        System.out.println("Aqui tendria que meterse el init");
-        //go to a la linea de la funcion
+        // PrintPOS[0]="1";//PUSH
+        // PrintPOS[1]=; tipo de retorno de la funcion
+        // PrintPOS[2]= " "; valor de la variable temporal de retorno que se modificara
+        // cuando se devuelva de la funcion
+        // Tripleeeee.add(PrintPOS);
+        // for de .getNumerosDeinpu
+        // PrintPOS[0]="1"
+        // PrintPOS[1]=; tipo de cada variable
+        // PrintPOS[2]=; valor de las variables
+        // Tripleeeee.add(PrintPOS);
+        // System.out.println("He acabado de hacer la preparacion de las variables");
+        // System.out.println("Aqui tendria que meterse el init");
+        // go to a la linea de la funcion
 
         return visitChildren(ctx);
     }
@@ -206,16 +221,17 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
     public String visitTip(GramProgParser.TipContext ctx) {
 
         if (ctx.TIPO_BOOL() != null) {
-            // System.out.println(ctx.TIPO_BOOL().toString());
-            luls = (ctx.TIPO_BOOL().toString());
+            luls = "3";
         } else if (ctx.TIPO_CADENA() != null) {
-            // System.out.println(ctx.TIPO_CADENA().toString());
-            luls = (ctx.TIPO_CADENA().toString());
+            luls = "2";
         } else if (ctx.TIPO_NUMERO() != null) {
-            // System.out.println(ctx.TIPO_NUMERO().toString());
             luls = (ctx.TIPO_NUMERO().toString());
+            if (luls.contains(".")) {
+                luls = "1";
+            } else {
+                luls = "0";
+            }
         } else {
-            // System.out.println(ctx.TIPO_VOID().toString());
             luls = (ctx.TIPO_VOID().toString());
         }
 
@@ -223,6 +239,28 @@ public class CuartaPasada extends GramProgBaseVisitor<String> {
     }
 
     public String visitExpr(GramProgParser.ExprContext ctx) {
+
+  
+        // tripleta temporal
+        if (ctx.SUM() != null) {
+            
+        String[] PrintPOS = { " ", " ", " " };
+        PrintPOS[0] = "1";
+        PrintPOS[1] = "0";
+        PrintPOS[2] = " ";
+        Tripleeeee.add(PrintPOS);
+        String[] PrintPOS1 = { " ", " ", " " };
+        PrintPOS1[0] = "1";
+        PrintPOS1[1] = "0";
+        PrintPOS1[2] = ctx.expr(0).getText();
+        Tripleeeee.add(PrintPOS1);
+        String[] PrintPOS2 = { " ", " ", " " };
+        PrintPOS2[0] = "5";
+        PrintPOS2[1] = "0";
+        PrintPOS2[2] =(ctx.expr(1).getText());
+        Tripleeeee.add(PrintPOS1);
+        
+        }
         return visitChildren(ctx);
     }
 
